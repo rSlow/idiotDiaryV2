72602435a07ec8060c3bc042c12194ceb9987b84
rslow <redmaxx4@gmail.com>
2023-10-17 00:33:33 +1000

    add cancel command, logger

--- telegram_bot/common/handlers.py
+++ telegram_bot/common/handlers.py
@@ -1,7 +1,7 @@
 import logging
 
 from aiogram import types, Router, F
-from aiogram.filters import Command, ExceptionTypeFilter
+from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 
 from .FSM import CommonState
@@ -11,18 +11,13 @@ first_router = Router()
 last_router = Router()
 
 
-@first_router.message(Command("start"))
+@first_router.message(Command("start", "cancel"))
 async def start(message: types.Message, state: FSMContext, text: str | None = None):
     await state.set_state(CommonState.start)
     await message.answer(
         text=text or "Куда надо?",
         reply_markup=StartKeyboard.build()
     )
-
-
-@last_router.message(F.text)
-async def delete(message: types.Message):
-    await message.delete()
 
 
 @first_router.error(F.update.message.as_("message"))
@@ -47,3 +42,8 @@ async def key_error_pass(event: types.ErrorEvent, message: types.Message, state:
         msg="",
         exc_info=event.exception
     )
+
+
+@last_router.message(F.text)
+async def delete(message: types.Message):
+    await message.delete()
--- telegram_bot/config/logger.py
+++ telegram_bot/config/logger.py
@@ -1,8 +1,16 @@
 import logging
+import os
+
+from config import settings
 
 
 def init_logging():
+    logs_folder = "logs"
+    if not (settings.BASE_DIR / logs_folder).is_dir():
+        os.mkdir(logs_folder)
+
     logging.basicConfig(
         level=logging.INFO,
+        filename=settings.BASE_DIR / logs_folder / "log.log",
         format="[%(asctime)s - %(levelname)s] %(name)s - %(message)s",
     )
