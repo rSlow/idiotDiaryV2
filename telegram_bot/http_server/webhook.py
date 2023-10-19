4928d12f436d0ddc2ddafc73ee29355ff41f62c2
rslow <redmaxx4@gmail.com>
2023-10-19 13:07:24 +1000

    remove fastapi imports

--- telegram_bot/http_server/webhook.py
+++ telegram_bot/http_server/webhook.py
@@ -1,8 +1,6 @@
 import logging
-from typing import Any
 
 from aiogram import Bot, Dispatcher
-from fastapi import FastAPI
 
 from config import settings
 
@@ -14,21 +12,3 @@ async def init_webhook(dp: Dispatcher, bot: Bot):
         secret_token=settings.WEBHOOK_SECRET
     )
     logging.info("SET WEBHOOK")
-
-
-def fastapi_setup(fastapi_app: FastAPI, dispatcher: Dispatcher, /, **kwargs: Any) -> None:
-    workflow_data = {
-        "app": fastapi_app,
-        "dispatcher": dispatcher,
-        **dispatcher.workflow_data,
-        **kwargs,
-    }
-
-    async def on_startup(*_: Any, **__: Any):
-        await dispatcher.emit_startup(**workflow_data)
-
-    async def on_shutdown(*_: Any, **__: Any):
-        await dispatcher.emit_shutdown(**workflow_data)
-
-    fastapi_app.add_event_handler("startup", on_startup)
-    fastapi_app.add_event_handler("shutdown", on_shutdown)
